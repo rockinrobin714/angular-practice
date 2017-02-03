10e140bb7f0fa8ac5e768404c0ac170fca27814f
rockinrobin714 <robindykema@gmail.com>
2017-02-02 23:29:54 -0600

    configuring angular materials

--- ngClassifieds/scripts/app.js
+++ ngClassifieds/scripts/app.js
@@ -1,1 +1,7 @@
-angular.module("ngClassifieds", [])
+angular
+	.module("ngClassifieds", ["ngMaterial"])
+	.config(funciton($mdThemingProvider) {
+		$mdThemimgProvider.theme('default')
+			.primaryPalette('teal')
+			.accentPallette('orange')
+	})
