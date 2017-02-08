6238dcdd97cd7a99f9d04739e5c23bf08f11bf92
rockinrobin714 <robindykema@gmail.com>
2017-02-08 11:45:25 -0600

    Added category filter

--- ngClassifieds/scripts/app.js
+++ ngClassifieds/scripts/app.js
@@ -1,12 +1,13 @@
-angular
-	.module("ngClassifieds", ["ngMaterial"])
-	.config(function($mdThemingProvider) {
-		$mdThemingProvider.theme('default')
-			.primaryPalette('teal')
-			.accentPalette('orange')
-	})
-	.directive("helloWorld", function(){
-		return {
-			template: "<h1>{{message}}</h1>"
-		}
-	});
+(function() {
+
+  "use strict";
+
+  angular
+    .module('ngClassifieds', ['ngMaterial'])
+    .config(function($mdThemingProvider) {
+      $mdThemingProvider.theme('default')
+        .primaryPalette('teal')
+        .accentPalette('orange');
+    });
+        
+})();
