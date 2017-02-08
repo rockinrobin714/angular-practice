a39cfd880c01ab9e500248f242993618403ff639
rockinrobin714 <robindykema@gmail.com>
2017-02-07 23:58:02 -0600

    user can save an edit

--- ngClassifieds/components/classifieds.ctr.js
+++ ngClassifieds/components/classifieds.ctr.js
@@ -26,17 +26,27 @@
 					$scope.classifieds.push(classified)
 					$scope.classified = {};
 					$scope.closeSidebar();
-					$mdToast.show(
-						$mdToast.simple()
-							.content("Classified saved!")
-							.position('top right')
-							.hideDelay(3000)
-						);
+					showToast('Classified saved')
 				}
 			}
-			$scope.editClassified = function(){
+			$scope.editClassified = function(classified){
 				$scope.editing = true;
 				$scope.openSidebar();
+				$scope.classified = classified;
+			}
+			$scope.savedEdit = function(){
+				$scope.editing = false;
+				$scope.classified = {};
+				$scope.closeSidebar();
+				showToast('Edit was saved')
+			}
+			function showToast(message) {
+				$mdToast.show(
+					$mdToast.simple()
+						.content(message)
+						.position('top right')
+						.hideDelay(3000)
+					);
 			}	
 		})
 })();
