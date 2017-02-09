a433667a2188a5bced45365908f63af7d930f666
rockinrobin714 <robindykema@gmail.com>
2017-02-09 14:13:04 -0600

    Set up new template

---
+++ ngClassifieds/components/classifieds/new/classifieds.new.ctr.js
@@ -0,0 +1,9 @@
+(function(){
+	"use strict"
+
+	angular
+		.module('ngClassifieds')
+		.controller('newClassifiedCtrl', function($mdSidenav, $mdDialog, classifiedsFactory){
+			var vm = this;
+		})
+})();
--- ngClassifieds/scripts/app.js
+++ ngClassifieds/scripts/app.js
@@ -10,5 +10,10 @@ angular
       		templateUrl: 'components/classifieds/classifieds.tpl.html',
       		controller: 'classifiedsCtrl as vm'
       	})
+      	.state('classifieds.new', {
+      		url: '/new',
+      		templateUrl: 'components/classifieds/new/classifieds.new.tpl.html',
+      		controller: 'newclassifiedsCtrl as vm'
+      	})
     })
 
