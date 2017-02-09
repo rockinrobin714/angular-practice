7be8a4eaa0da2bcbafa95ed43ffb5c5bcc946095
rockinrobin714 <robindykema@gmail.com>
2017-02-08 21:19:00 -0600

    Nesting state with UI router

--- ngClassifieds/scripts/app.js
+++ ngClassifieds/scripts/app.js
@@ -3,11 +3,24 @@
   "use strict";
 
   angular
-    .module('ngClassifieds', ['ngMaterial'])
-    .config(function($mdThemingProvider) {
+    .module('ngClassifieds', ['ngMaterial', 'ui.router'])
+    .config(function($mdThemingProvider, $stateProvider) {
       $mdThemingProvider.theme('default')
         .primaryPalette('teal')
         .accentPalette('orange');
+      $stateProvider
+      	.state('stateone', {
+      		url: '/stateone',
+      		template: '<h1>State One</h1>'
+      	})
+      	.state('statetwo', {
+      		url: '/statetwo',
+      		template: '<h1>State Two</h1> <md-button ui-sref="statetwo.more">Go to nested state</md-button>'
+      	})
+      	.state('statetwo.more', {
+      		url: '/more',
+      		template: '<p>This is the deeper state</p>'
+      	});
     });
         
 })();
