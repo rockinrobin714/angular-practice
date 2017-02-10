b5c1bd487c0719a8f0e68b3289c219fad072612d
rockinrobin714 <robindykema@gmail.com>
2017-02-09 19:45:41 -0600

    Debugging

--- ngClassifieds/scripts/app.js
+++ ngClassifieds/scripts/app.js
@@ -1,19 +1,37 @@
-angular
+(function() {
+
+  "use strict";
+
+  angular
     .module('ngClassifieds', ['ngMaterial', 'ui.router'])
-    .config(function($mdThemingProvider, $stateProvider) {
+    .config(function($mdThemingProvider, $stateProvider, $urlRouterProvider) {
+
       $mdThemingProvider.theme('default')
         .primaryPalette('teal')
         .accentPalette('orange');
+
+      $urlRouterProvider.otherwise('/classifieds');
+
       $stateProvider
-      	.state('classifieds', {
-      		url: '/classifieds',
-      		templateUrl: 'components/classifieds/classifieds.tpl.html',
-      		controller: 'classifiedsCtrl as vm'
-      	})
-      	.state('classifieds.new', {
-      		url: '/new',
-      		templateUrl: 'components/classifieds/new/classifieds.new.tpl.html',
-      		controller: 'newclassifiedsCtrl as vm'
-      	})
-    })
+        .state('classifieds', {
+          url: '/classifieds',
+          templateUrl: 'components/classifieds/classifieds.tpl.html',
+          controller: 'classifiedsController as vm'
+        })
+        .state('classifieds.new', {
+          url: '/new',
+          templateUrl: 'components/classifieds/new/classifieds.new.tpl.html',
+          controller: 'newClassifiedsController as vm'
+        })
+        .state('classifieds.edit', {
+          url: '/:id/edit',
+          templateUrl: 'components/classifieds/edit/classifieds.edit.tpl.html',
+          controller: 'editClassifiedsController as vm',
+          params: {
+            classified: null
+          }
+        });
+    });
+    
+})();
 
